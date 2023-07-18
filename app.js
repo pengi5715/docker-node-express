3ba5cabecbd6863240f2fb9e8848bcb2b36ec025
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-18 18:27:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,59 @@ app.get('/', (req, res) => {
   options.params.query = query;
   // axios로 API 요청
   axios.request(options).then(function (response) {
+    // items 배열에서 원하는 속성만 추출
+    const items = response.data.items.map(item => ({
+      책제목: item.title,
+      링크: item.link,
+      사진: item.image,
+      저자: item.author,
+      가격: item.discount,
+      출판사: item.publisher,
+      출간일: item.pubdate
+    }));
+    res.send(items);
+  }).catch(function (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+/*--------------------------------------------기존 적용
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
+
+// options 객체에서 params 속성을 추가
+const options = {
+  'method': 'GET',
+  'url': 'https://openapi.naver.com/v1/search/book.json',
+  'headers': {
+    'X-Naver-Client-Id': 'j8PIiTpiuPi6PzcQD4MV',
+    'X-Naver-Client-Secret': 'DuZ9KkT_ZH'
+  },
+  'params': {}
+};
+
+app.get('/', (req, res) => {
+  // req.query에서 query 파라미터를 받아옴
+  const query = req.query.query;
+  // query 파라미터가 없으면 에러 메시지 전송
+  if (!query) {
+    res.status(400).send('query parameter is required');
+    return;
+  }
+  // query 파라미터를 options 객체의 params 속성에 추가
+  options.params.query = query;
+  // axios로 API 요청
+  axios.request(options).then(function (response) {
     res.send(response.data);
   }).catch(function (error) {
     console.error(error);
@@ -37,7 +90,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-/* axios.post 함수 추가 -> intellibook.org 로 데이터 전송 
+/*---------------------------------- axios.post 함수 추가 -> intellibook.org 로 데이터 전송 
 const express = require('express');
 const app = express();
 const port = 3000;
