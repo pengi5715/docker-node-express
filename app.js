d687b8d1aaffa8d611c0eca729e90bc013acd786
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-18 18:12:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -37,23 +37,21 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-/*
+/* axios.post 함수 추가 -> intellibook.org 로 데이터 전송 
 const express = require('express');
-const request = require('request');
 const app = express();
 const port = 3000;
-const axios = require('axios'); //추가
+const axios = require('axios');
 
-// options 객체에서 params 속성을 제거
+// options 객체에서 params 속성을 추가
 const options = {
   'method': 'GET',
-  //'url': 'https://openapi.naver.com/v1/search/book_adv',
   'url': 'https://openapi.naver.com/v1/search/book.json',
   'headers': {
     'X-Naver-Client-Id': 'j8PIiTpiuPi6PzcQD4MV',
     'X-Naver-Client-Secret': 'DuZ9KkT_ZH'
-  }
+  },
+  'params': {}
 };
 
 app.get('/', (req, res) => {
@@ -64,12 +62,22 @@ app.get('/', (req, res) => {
     res.status(400).send('query parameter is required');
     return;
   }
-  // query 파라미터를 options 객체에 추가
-  //options.params = {query};
-  options.qs = {query};
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.send(response.body);
+  // query 파라미터를 options 객체의 params 속성에 추가
+  options.params.query = query;
+  // axios로 API 요청
+  axios.request(options).then(function (response) {
+    // intellibook.org 서버로 데이터 전송
+    axios.post('https://intellibook.org/api/data', response.data)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    res.send(response.data);
+  }).catch(function (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
   });
 });
 
