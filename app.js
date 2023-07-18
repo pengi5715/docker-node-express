1db9f7573f52532059a213d7f5ac139fe9aa2450
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-18 15:26:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-/*
 //서버 실행+naver-api.js
 const express = require('express');
 const request = require('request');
 const app = express();
 const port = 3000;
+const axios = require('axios'); //추가
 
 const options = {
   'method': 'GET',
   //'url': 'https://openapi.naver.com/v1/search/book_adv',
   'url': 'https://openapi.naver.com/v1/search/book.json',
-  'params': { query: '', display: 10, start: 1 }
+  'params': { query: 'YOUR_QUERY', display: 10, start: 1 } //추가
   'headers': {
     'X-Naver-Client-Id': 'j8PIiTpiuPi6PzcQD4MV',
     'X-Naver-Client-Secret': 'DuZ9KkT_ZH'
@@ -26,37 +26,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-*/
-const express = require('express');
-const request = require('request');
-const app = express();
-const port = 3000;
-const axios = require('axios');
 
-const CLIENT_ID = 'j8PIiTpiuPi6PzcQD4MV';
-const CLIENT_SECRET = 'DuZ9KkT_ZH';
-
-const options = {
-  method: 'GET',
-  url: 'https://openapi.naver.com/v1/search/book.json',
-  params: { query: 'YOUR_QUERY', display: 10, start: 1 },
-  headers: { 'X-Naver-Client-Id': CLIENT_ID, 'X-Naver-Client-Secret': CLIENT_SECRET }
-};
-
+//추가
 axios.request(options).then(function (response) {
   console.log(response.data);
 }).catch(function (error) {
   console.error(error);
+});
 
-app.get('/', (req, res) => {
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.send(response.body);
-});
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  
-});
 
 
 
