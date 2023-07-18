7715a9369ad43c544cc349389bd708439e830e0d
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-18 16:17:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 3000;
+const axios = require('axios'); //추가
+
+// options 객체에서 params 속성을 제거
+const options = {
+  'method': 'GET',
+  //'url': 'https://openapi.naver.com/v1/search/book_adv',
+  'url': 'https://openapi.naver.com/v1/search/book.json',
+  'headers': {
+    'X-Naver-Client-Id': 'j8PIiTpiuPi6PzcQD4MV',
+    'X-Naver-Client-Secret': 'DuZ9KkT_ZH'
+  }
+};
+
+app.get('/', (req, res) => {
+  // req.query에서 query 파라미터를 받아옴
+  const query = req.query.query;
+  // query 파라미터가 없으면 에러 메시지 전송
+  if (!query) {
+    res.status(400).send('query parameter is required');
+    return;
+  }
+  // query 파라미터를 options 객체에 추가
+  options.params = {query};
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.send(response.body);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//추가
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+
+/*
 //서버 실행+naver-api.js
 const express = require('express');
 const request = require('request');
@@ -33,22 +79,4 @@ axios.request(options).then(function (response) {
 }).catch(function (error) {
   console.error(error);
 });
-
-
-
-
-
-/*  삭제해도 관계없을
-const express = require('express')
-const axios = require('axios') //추
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello, Cloudtype!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 */
