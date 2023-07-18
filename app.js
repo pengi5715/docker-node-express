93d99de644dc2605971c56265fc4cff0751a1740
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-18 15:17:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 //서버 실행+naver-api.js
 const express = require('express');
 const request = require('request');
@@ -8,6 +9,7 @@ const options = {
   'method': 'GET',
   //'url': 'https://openapi.naver.com/v1/search/book_adv',
   'url': 'https://openapi.naver.com/v1/search/book.json',
+  'params': { query: '', display: 10, start: 1 }
   'headers': {
     'X-Naver-Client-Id': 'j8PIiTpiuPi6PzcQD4MV',
     'X-Naver-Client-Secret': 'DuZ9KkT_ZH'
@@ -24,12 +26,30 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+*/
+
+const axios = require('axios');
+
+const CLIENT_ID = 'j8PIiTpiuPi6PzcQD4MV';
+const CLIENT_SECRET = 'DuZ9KkT_ZH';
+
+const options = {
+  method: 'GET',
+  url: 'https://openapi.naver.com/v1/search/book.json',
+  params: { query: 'YOUR_QUERY', display: 10, start: 1 },
+  headers: { 'X-Naver-Client-Id': CLIENT_ID, 'X-Naver-Client-Secret': CLIENT_SECRET }
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
 
 
 
 
-
-/*
+/*  삭제해도 관계없을
 const express = require('express')
 const axios = require('axios') //추
 const app = express()
